e6449996ee69931aed46a8c70115a5b713a07c11
Chloe Jackson <jacksonc@vmware.com>
2013-11-11 15:28:19 -0800

    Had to move assets

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Cfcommunity::Application.routes.draw do
   ComfortableMexicanSofa::Routing.admin(:path => '/cms-admin')
 
   match '/' => 'home#index'
-  match '/individualcontribution.pdf' => redirect('/assets/individualcontribution.pdf')
-  match '/corpcontribution.pdf' => redirect('/assets/corpcontribution.pdf')
+  match '/individualcontribution.pdf' => redirect('http://blog.cloudfoundry.com/wp-content/static/cforg/pdfs/individualcontribution.pdf')
+  match '/corpcontribution.pdf' => redirect('http://blog.cloudfoundry.com/wp-content/static/cforg/pdfs/corpcontribution.pdf')
   
   # Make sure this routeset is defined last
   ComfortableMexicanSofa::Routing.content(:path => '/', :sitemap => false)
