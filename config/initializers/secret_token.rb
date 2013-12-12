933e11a81c9a9ca368252627055a09d05c67dad4
Vince Brunssen <brunssen@us.ibm.com>
2013-12-11 18:36:44 -0600

    Update cloudfoundry.org to be the new structure of an HTML/CSS/JavaScript structure instead of using Rails

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Cfcommunity::Application.config.secret_token = '8cb62570a6fb4fd5dc29979f85a6e945b2c7acc7c19afc7d9ad3b28707da65c8e6c25cdba0742f8ad6719cc1c2769b0ecf26a2c77f2d3d95e04d9a8d2a7505ac'
