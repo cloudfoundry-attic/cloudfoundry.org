aaba73d8a8f4b062bea164750407d10af027b5e7
jacksoncvm <cjackson@pivotal.io>
2015-01-26 15:17:26 -0800

    Temporary fix for blog and github feeds on homepage

--- middleman/source/javascripts/feeds.js
+++ middleman/source/javascripts/feeds.js
@@ -1,9 +1,9 @@
 function loadFeedControl() {
-  var feed = "http://blog.cloudfoundry.org//feed?"+new Date().getTime();
+  var feed = "http://blog.cloudfoundry.org/feed";
   var fg = new GFdynamicFeedControl(feed, "updates");
-  var feed2 = "https://groups.google.com/a/cloudfoundry.org/forum/feed/vcap-dev/msgs/atom_v1_0.xml?"+new Date().getTime();
+  var feed2 = "https://groups.google.com/a/cloudfoundry.org/forum/feed/vcap-dev/msgs/atom_v1_0.xml";
   var fg2 = new GFdynamicFeedControl(feed2, "discussions");
-  var feed3 = "https://github.com/cloudfoundry/bosh/commits/master.atom?"+new Date().getTime();
+  var feed3 = "https://github.com/cloudfoundry/bosh/commits/master.atom";
   var fg3 = new GFdynamicFeedControl(feed3, "commits");
 }
 google.load("feeds", "1");
